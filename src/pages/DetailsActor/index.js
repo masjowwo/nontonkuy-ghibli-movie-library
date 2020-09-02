ca26c9e8672193ad5cf4ef76b7141c5aa36617f1
Riky Yudistira <ryrdn25@gmail.com>
2020-09-02 13:51:10 +0700

    fix bug detail actors

--- src/pages/DetailsActor/index.js
+++ src/pages/DetailsActor/index.js
@@ -16,7 +16,7 @@ class DetailsActor extends Component {
 
 
     const breadcrumbList = [
-      {pageTitle: "Home", pageHref: ""},
+      {pageTitle: "Actors", pageHref: "/actors"},
       {pageTitle: "Actor Details", pageHref: ""}
     ] 
 
